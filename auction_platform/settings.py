863a2598911bb6cc1239d35c037cc8c0d61f428b
sampath2526 <nagasmpth@gmail.com>
2025-02-12 23:47:47 +0530

    1

--- auction_platform/settings.py
+++ auction_platform/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'auction-platform.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'auction-platform-rfg1.onrender.com').split(',')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
